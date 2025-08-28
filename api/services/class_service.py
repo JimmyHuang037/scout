6ef64d6094ba47837d6cfc86c9248ca2df977f4c
jimmy <jimmy@home>
2025-08-28 21:00:21 +0800

    update

--- api/services/class_service.py
+++ api/services/class_service.py
@@ -2,6 +2,11 @@
 from utils import database_service
 
 
+class ClassNotFoundError(Exception):
+    """班级未找到异常"""
+    pass
+
+
 class ClassService:
     """班级服务类"""
     
@@ -145,37 +150,37 @@ class ClassService:
     
     def delete_class(self, class_id):
         """
-        删除班级（同时删除相关的学生和成绩）
-        
-        Args:
-            class_id (int): 班级ID
-            
-        Returns:
-            bool: 是否删除成功
+        删除班级
+        :param class_id: 班级ID
+        :return: None
         """
+        # 检查班级是否存在
+        class_info = self.get_class_by_id(class_id)
+        if not class_info:
+            raise ClassNotFoundError(f"班级ID {class_id} 不存在")
+
         try:
             # 开始事务
             self.db_service.start_transaction()
-            
+
             # 删除与该班级相关的成绩
-            delete_scores_query = """
-                DELETE s FROM Scores s
-                INNER JOIN Students st ON s.student_id = st.student_id 
-                WHERE st.class_id = %s
-            """
-            self.db_service.execute_update(delete_scores_query, (class_id,))
+            delete_scores_query = "DELETE FROM Scores WHERE student_id IN (SELECT student_id FROM Students WHERE class_id = %s)"
+            self.db_service.cursor.execute(delete_scores_query, (class_id,))
+            
+            # 删除与该班级相关的教师班级关联
+            delete_teacher_classes_query = "DELETE FROM TeacherClasses WHERE class_id = %s"
+            self.db_service.cursor.execute(delete_teacher_classes_query, (class_id,))
             
             # 删除与该班级相关的学生
             delete_students_query = "DELETE FROM Students WHERE class_id = %s"
-            self.db_service.execute_update(delete_students_query, (class_id,))
+            self.db_service.cursor.execute(delete_students_query, (class_id,))
             
             # 删除班级
             delete_class_query = "DELETE FROM Classes WHERE class_id = %s"
-            self.db_service.execute_update(delete_class_query, (class_id,))
-            
+            self.db_service.cursor.execute(delete_class_query, (class_id,))
+
             # 提交事务
             self.db_service.commit()
-            return True
         except Exception as e:
             # 回滚事务
             self.db_service.rollback()
