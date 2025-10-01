220091388afbf6f4849f60c3be17defa024416bc
jimmy <jimmy@home>
2025-10-01 12:09:44 +0800

    update

--- api/apps/blueprints/admin/teacher_classes/__init__.py
+++ api/apps/blueprints/admin/teacher_classes/__init__.py
@@ -7,8 +7,8 @@ admin_teacher_classes_bp = Blueprint('admin_teacher_classes', __name__)
 from . import teacher_class_management
 
 # 注册路由
-admin_teacher_classes_bp.add_url_rule('/', methods=['POST'], view_func=teacher_class_management.create_teacher_class)
 admin_teacher_classes_bp.add_url_rule('/', methods=['GET'], view_func=teacher_class_management.get_teacher_classes)
+admin_teacher_classes_bp.add_url_rule('/', methods=['POST'], view_func=teacher_class_management.create_teacher_class)
 admin_teacher_classes_bp.add_url_rule('/<int:teacher_class_id>', methods=['GET'], view_func=teacher_class_management.get_teacher_class)
 admin_teacher_classes_bp.add_url_rule('/<int:teacher_class_id>', methods=['PUT'], view_func=teacher_class_management.update_teacher_class)
 admin_teacher_classes_bp.add_url_rule('/<int:teacher_class_id>', methods=['DELETE'], view_func=teacher_class_management.delete_teacher_class)
