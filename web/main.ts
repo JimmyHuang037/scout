456e53c0f29e0bc306f0c34229ffcabad547fdd3
william <william@home>
2025-10-19 13:07:00 +0800

    update

--- web/main.ts
+++ web/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { AppComponent } from './src/student/app/app';
-import { routes } from './src/student/app/app.routes';
+import { AppComponent } from './src/app/app';
+import { routes } from './src/app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
