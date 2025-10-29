c90b0b75cceb31f8a1d60768125dbcece11bf177
william <william@home>
2025-10-29 19:48:11 +0800

    update

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent as StudentDashboardComponent } from '../student/dashboard.component';
 import { TeacherDashboardComponent } from '../teacher/dashboard.component';
+import { LoginComponent } from '../auth/login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/student/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'student/dashboard', component: StudentDashboardComponent },
   { path: 'teacher/dashboard', component: TeacherDashboardComponent },
-  { path: '**', redirectTo: '/student/dashboard' }
+  { path: '**', redirectTo: '/login' }
 ];
